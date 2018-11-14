be236302c6563a82257c795f01a81ad840ae6f14
hewei <heweimailbox@foxmail.com>
2018-11-14 19:31:26 +0800

    regular update

--- src/views/welcome/Welcome.vue
+++ src/views/welcome/Welcome.vue
@@ -17,7 +17,18 @@ export default {
     mounted () {
         // this.name(1, 2)
         // getNames().then((res) => { console.log(res) }).catch((err) => { console.log(err) })
-
+        try {
+            throw new Error('Hello', 'someFile.js', 10)
+        } catch (e) {
+            // console.log(e instanceof ReferenceError) // true
+            // console.log(e.message) // "Hello"
+            // console.log(e.name) // "ReferenceError"
+            // console.log(e.fileName) // "someFile.js"
+            // console.log(e.lineNumber) // 10
+            // console.log(e.columnNumber) // 0
+            // console.log(e.stack) // "@Scratchpad/2:2:9\n"
+            console.log(e) // "@Scratchpad/2:2:9\n"
+        }
     },
 
     methods: {
