bfd8bb5b0f464cd0ad8217a9f889d44f951faf62
hewei <heweimailbox@foxmail.com>
2019-02-12 18:11:47 +0800

    regular update

--- src/utils/commonFn.js
+++ src/utils/commonFn.js
@@ -9,3 +9,31 @@ export function formatToQueryString (obj) {
 
     return temp.join('&')
 }
+
+export function formatTime (dateString, type) {
+    if (dateString) {
+        type = type || 'precisionM'
+
+        const date = new Date(dateString)
+        let y = date.getFullYear(), m = date.getMonth() + 1, d = date.getDate(), h = date.getHours(), minute = date.getMinutes(), s = date.getSeconds()
+
+        if (m < 10) m = '0' + m
+        if (h < 10) h = '0' + h
+        if (minute < 10) minute = '0' + minute
+        if (s < 10) s = '0' + s
+
+        const commandObj = {
+            precisionM: function () {
+                return y + '-' + m + '-' + d + '-' + ' ' + h + ':' + m
+            },
+            dateZH: function () {
+                const temp = dateString.split(' ')[0].split('-')
+                return `${temp[0]}年${temp[1]}月${temp[2]}日`
+            }
+        }
+
+        return commandObj[type]()
+    } else {
+        return ''
+    }
+}
