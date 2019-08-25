84784736f9a33a9c171b04ae441d2c44a60ce1a6
hewei <heweimailbox@foxmail.com>
2019-08-25 18:27:55 +0800

    regular update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,29 @@
 module.exports = {
-    "root": true,
-        "env": {
-        "node": true
-    },
-    "extends": [
-        "plugin:vue/essential",
-        "@vue/standard"
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  "rules": {
+    // 允许使用console
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "indent": [
+      "error",
+      4
     ],
-    "rules": {
-<<<<<<< HEAD
-        // 允许使用console
-        "no-console": "off",
-=======
->>>>>>> b6229c826b2a6bebd216fc265a8796b9567b949f
-        "indent": [
-            "error",
-            4
-        ],
-        "one-var": [
-            "error",
-            {
-                // 对已经初始化的变量，要求其连续的变量声明合并为一个声明
-                "initialized": "consecutive"
-            }
-        ]
-    },
-    "parserOptions": {
-        "parser": "babel-eslint"
-    }
+    "one-var": [
+      "error",
+      {
+        // 对已经初始化的变量，要求其连续的变量声明合并为一个声明
+        "initialized": "consecutive"
+      }
+    ]
+  },
 }
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+    /**
+     * @des 和webpack的一致
+     * 这里用./目的是为了让打包后的静态文件可以直接运行
+     */
+    publicPath: './',
+
     devServer: {
         compress: true,
         port: 7878,
@@ -17,15 +23,12 @@ module.exports = {
         }
     },
 
-    // 修改打包后的地址
-    baseUrl: './',
-
     // 是否每次保存时 lint 代码，错误输出为编译警告
-    lintOnSave: false,
+    lintOnSave: false
 
-    pages: {
-        index: {
-            title: '这是配置的标题'
-        }
-    }
+    // pages: {
+    //     index: {
+    //         title: '这是配置的标题'
+    //     }
+    // }
 }
