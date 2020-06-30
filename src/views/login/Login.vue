0b1003d7b59ede4e4c3797e53d5a3739eb6a7144
warren <heweimailbox@foxmail.com>
2020-06-30 17:21:17 +0800

    regular update

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -2,12 +2,13 @@
     <div class="login-part">
         <el-row type="flex" justify="center">
             <el-col :span="5">
-                <el-form :model="formData" status-icon :rules="rules" ref="formLogin">
+                <el-form :model="formData" :rules="rules" ref="formLogin">
                     <el-form-item prop="username">
                         <el-input type="text" v-model="formData.username" placeholder="username"></el-input>
                     </el-form-item>
                     <el-form-item prop="password">
-                        <el-input type="password" v-model="formData.password" placeholder="password"></el-input>
+                        <el-input :type="eyeStatus? 'text' : 'password'" v-model="formData.password" placeholder="password"></el-input>
+                        <div :class="eyeStatus? 'eye-open': 'eye-close'" @click="handleEye"></div>
                     </el-form-item>
                     <el-checkbox class="auto-login" v-model="autoLogin">remember login</el-checkbox>
                     <el-form-item>
@@ -37,6 +38,7 @@ export default {
         return {
             autoLogin: false,
             MD5SHA1: '',
+            eyeStatus: false,
             formData: {
                 username: '',
                 password: ''
@@ -80,6 +82,11 @@ export default {
     methods: {
         ...mapActions(['actionSetProjectName']),
         ...mapActions('user', ['actionSetUserInfo']),
+
+        handleEye () {
+            this.eyeStatus = !this.eyeStatus
+        },
+
         submitForm () {
             this.$refs.formLogin.validate((valid) => {
                 if (valid) {
