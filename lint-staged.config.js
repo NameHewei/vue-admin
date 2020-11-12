4a42462b50c21e9db02cd2156cbf303500604302
hewei <hewei@gmail.com>
2020-11-12 08:59:45 +0800

    regular update

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     '**/*.js?(x)': (allStagedFiles) => {
         if (allStagedFiles.length) {
-            return 'printf Script files arent allowed in src directory'
+            return `printf Script files arent allowed in src directory: ${allStagedFiles.length}`
         }
         return ['npm run cusPre', 'vue-cli-service lint']
     }
